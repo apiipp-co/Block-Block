d23b4f2065d4a604948141549c911e5f02b7344b
Adhitya Afif Ardana <168333169+apiipp-co@users.noreply.github.com>
2026-07-15 23:05:53 +0700

    Update db.js

--- Downloads/BlockBlog-main/backend/config/db.js
+++ Downloads/BlockBlog-main/backend/config/db.js
@@ -1,22 +1,13 @@
 const mongoose = require("mongoose");
 
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
 const connectDB = async () => {
-  if (cached.conn) return cached.conn;
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.MONGO_URI, {
-      serverSelectionTimeoutMS: 10000,
-    });
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Atlas connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`MongoDB connection error: ${error.message}`);
+    process.exit(1);
   }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
 };
 
 module.exports = connectDB;
