f709cbf646fb11eff49136ec84b155333ffefd75
Adhitya Afif Ardana <168333169+apiipp-co@users.noreply.github.com>
2026-07-15 22:16:57 +0700

    Enhance MongoDB connection logic and error handling
    Added check for existing MongoDB connection and modified error handling.

--- Downloads/BlockBlog-main/backend/config/db.js
+++ Downloads/BlockBlog-main/backend/config/db.js
@@ -2,11 +2,17 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    if (mongoose.connection.readyState === 1) {
+      return;
+    }
+
     const conn = await mongoose.connect(process.env.MONGO_URI);
+
     console.log(`MongoDB Atlas connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`MongoDB connection error: ${error.message}`);
-    process.exit(1);
+    console.error("MongoDB connection error:", error.message);
+
+    throw error; // jangan process.exit()
   }
 };
 
